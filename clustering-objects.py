bcf367ff3b8f935618d9603e10fd65830e97cd80
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-10-15 16:25:23 +0100

    Simplify

--- clustering-objects.py
+++ clustering-objects.py
@@ -1,12 +1,11 @@
-import qubalab
 from qubalab.qupath import qupath_gateway
-import numpy as np
 import pandas as pd
 import umap
+import leidenalg as la
+import igraph
+from sklearn.neighbors import kneighbors_graph
 
-token = None
-port = 25333
-gateway = qupath_gateway.create_gateway(auth_token=token, port=port)
+gateway = qupath_gateway.create_gateway(auth_token=None, port=25333)
 
 detections = gateway.getDetectionObjects()
 measurement_names = detections[0].getMeasurements().keySet()
@@ -14,21 +13,17 @@ measurement_names = [x for x in measurement_names if x.startswith("Cell") and x.
 
 df = pd.DataFrame(columns = measurement_names)
 for det in detections:
-    row = {name: det.getMeasurements().get(name) for name in measurement_names}
-    df = df._append(row, ignore_index=True)
+    df = df._append(
+        {name: det.getMeasurements().get(name) for name in measurement_names},
+        ignore_index=True)
 
 normalized_df = (df - df.mean()) / df.std()
 
 embedding = umap.UMAP().fit_transform(normalized_df)
 
-
 for i in range(embedding.shape[0]):
     detections[i].getMeasurementList().put("UMAP1", float(embedding[i][0]))
     detections[i].getMeasurementList().put("UMAP2", float(embedding[i][1]))
-
-import leidenalg as la
-import igraph
-from sklearn.neighbors import kneighbors_graph
 
 A = kneighbors_graph(normalized_df, 50)
 g = igraph.Graph.Adjacency((A > 0))
