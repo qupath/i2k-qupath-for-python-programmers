d6bcd9355793057e9e8457cdae29e5733c9ae041
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-10-17 17:52:59 +0100

    Run instanseg in python

--- clear.groovy
+++ clear.groovy
@@ -3,3 +3,10 @@ for (PathObject po: getDetectionObjects()) {
     po.getMeasurementList().remove("UMAP2")
     po.setPathClass(null)
 }
+for (PathObject po: getAnnotationObjects()) {
+    po.setName(null)
+    po.setPathClass(null)
+    if (po.getChildObjects().size() == 0) {
+        getCurrentHierarchy().removeObject(po, false)
+    }
+}
